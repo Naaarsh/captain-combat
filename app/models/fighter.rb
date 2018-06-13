1a96ebed2bbb5e92f2ed6a3942e79a3fe892283c
Simon Leyendecker <leyendecker.simon@gmail.com>
2018-06-13 12:46:27 +0200

    Fighters Validation

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,2 +1,14 @@
 class Fighter < ApplicationRecord
+  validates :name, presence: true
+  validates :life, :attack, :dodge, :critical, inclusion:
+    { in: 0..10, message: "can't have less than 0 or more than 10 skillpoints" }
+
+  validate :cant_have_more_than_15_skillpoints
+
+  def cant_have_more_than_15_skillpoints
+    skillpoints = life + attack + dodge + critical
+    if skillpoints > 15
+      errors.add(:fighter, "can't have more than 15 skillpoints, current: #{skillpoints}")
+    end
+  end
 end
